2847e713f5829997045f8575cbd1be154d988940
90535 <90535@REY>
2023-06-06 23:43:11 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WinFormSnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel = new System.Windows.Forms.Panel();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.btnDurdur = new System.Windows.Forms.Button();
+            this.btnYeniOyun = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureDown = new System.Windows.Forms.PictureBox();
+            this.pictureRight = new System.Windows.Forms.PictureBox();
+            this.pictureUp = new System.Windows.Forms.PictureBox();
+            this.pictureLeft = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureUp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLeft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(420, 420);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnBaslat.FlatAppearance.BorderSize = 0;
+            this.btnBaslat.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBaslat.Location = new System.Drawing.Point(444, 27);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(74, 32);
+            this.btnBaslat.TabIndex = 2;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = false;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // btnDurdur
+            // 
+            this.btnDurdur.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnDurdur.FlatAppearance.BorderSize = 0;
+            this.btnDurdur.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDurdur.Location = new System.Drawing.Point(520, 27);
+            this.btnDurdur.Name = "btnDurdur";
+            this.btnDurdur.Size = new System.Drawing.Size(74, 32);
+            this.btnDurdur.TabIndex = 3;
+            this.btnDurdur.Text = "Durdur";
+            this.btnDurdur.UseVisualStyleBackColor = false;
+            this.btnDurdur.Click += new System.EventHandler(this.btnDurdur_Click);
+            // 
+            // btnYeniOyun
+            // 
+            this.btnYeniOyun.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnYeniOyun.FlatAppearance.BorderSize = 0;
+            this.btnYeniOyun.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnYeniOyun.Location = new System.Drawing.Point(444, 65);
+            this.btnYeniOyun.Name = "btnYeniOyun";
+            this.btnYeniOyun.Size = new System.Drawing.Size(150, 32);
+            this.btnYeniOyun.TabIndex = 4;
+            this.btnYeniOyun.Text = "Yeni Oyun";
+            this.btnYeniOyun.UseVisualStyleBackColor = false;
+            this.btnYeniOyun.Click += new System.EventHandler(this.btnYeniOyun_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Menu;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureDown, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pictureRight, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureUp, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureLeft, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(444, 138);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(150, 150);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // pictureDown
+            // 
+            this.pictureDown.Image = ((System.Drawing.Image)(resources.GetObject("pictureDown.Image")));
+            this.pictureDown.Location = new System.Drawing.Point(52, 101);
+            this.pictureDown.Name = "pictureDown";
+            this.pictureDown.Size = new System.Drawing.Size(44, 44);
+            this.pictureDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureDown.TabIndex = 2;
+            this.pictureDown.TabStop = false;
+            this.pictureDown.Click += new System.EventHandler(this.pictureDown_Click);
+            // 
+            // pictureRight
+            // 
+            this.pictureRight.Image = ((System.Drawing.Image)(resources.GetObject("pictureRight.Image")));
+            this.pictureRight.Location = new System.Drawing.Point(102, 52);
+            this.pictureRight.Name = "pictureRight";
+            this.pictureRight.Size = new System.Drawing.Size(45, 43);
+            this.pictureRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureRight.TabIndex = 3;
+            this.pictureRight.TabStop = false;
+            this.pictureRight.Click += new System.EventHandler(this.pictureRight_Click);
+            // 
+            // pictureUp
+            // 
+            this.pictureUp.Image = ((System.Drawing.Image)(resources.GetObject("pictureUp.Image")));
+            this.pictureUp.Location = new System.Drawing.Point(52, 3);
+            this.pictureUp.Name = "pictureUp";
+            this.pictureUp.Size = new System.Drawing.Size(44, 43);
+            this.pictureUp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureUp.TabIndex = 0;
+            this.pictureUp.TabStop = false;
+            this.pictureUp.Click += new System.EventHandler(this.pictureUp_Click);
+            // 
+            // pictureLeft
+            // 
+            this.pictureLeft.Image = ((System.Drawing.Image)(resources.GetObject("pictureLeft.Image")));
+            this.pictureLeft.Location = new System.Drawing.Point(3, 52);
+            this.pictureLeft.Name = "pictureLeft";
+            this.pictureLeft.Size = new System.Drawing.Size(43, 43);
+            this.pictureLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureLeft.TabIndex = 1;
+            this.pictureLeft.TabStop = false;
+            this.pictureLeft.Click += new System.EventHandler(this.pictureLeft_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.BackColor = System.Drawing.Color.SeaGreen;
+            this.lblScore.Location = new System.Drawing.Point(441, 405);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(153, 27);
+            this.lblScore.TabIndex = 6;
+            this.lblScore.Text = "0";
+            this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblScore.Click += new System.EventHandler(this.lblScore_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 450);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnDurdur);
+            this.Controls.Add(this.btnYeniOyun);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureUp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLeft)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.Button btnDurdur;
+        private System.Windows.Forms.Button btnYeniOyun;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureUp;
+        private System.Windows.Forms.PictureBox pictureLeft;
+        private System.Windows.Forms.PictureBox pictureDown;
+        private System.Windows.Forms.PictureBox pictureRight;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormSnakeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<KareInfo> listKare = new List<KareInfo>(); //tüm karelerin yerini tanımlayan bir list tanımlıyoruz
+#pragma warning disable CS0649 // 'Form1.listUzuv' alanı hiçbir zaman atanmaz ve her zaman varsayılan null değerine sahip olur
+        List<KareInfo> listUzuv;
+#pragma warning restore CS0649 // 'Form1.listUzuv' alanı hiçbir zaman atanmaz ve her zaman varsayılan null değerine sahip olur
+        YilanInfo yilanInfo=null;
+
+
+        int yon = 2; //(12 den başlayarak 1 den 4 e kadar önleri numaralandırdığımızda)
+        int toplamKareSayisi=900;
+        bool gameOver = false;
+        bool yemVar = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int kareKenarUzunluk = 12;
+            int kareX = 1;
+            int kareY = 1;
+            int margin = 2;
+
+
+            for (int i = 0; i < toplamKareSayisi; i++) //i= indis
+            {
+                KareInfo kareInfo = new KareInfo(this.panel, new Point(kareX, kareY), new Size(kareKenarUzunluk, kareKenarUzunluk), i);
+                listKare.Add(kareInfo);
+                // Kareyi oluştur ve göster
+                kareInfo.pictureBoxAdd();
+
+                kareX += kareKenarUzunluk + margin; //bir kare kadar kayarak ilerleyecek
+                if ((i+1)% 30==0) 
+                {
+                    kareX = 1;
+                    kareY += kareKenarUzunluk+margin;//29 ve katlarına gelince bir kare kadar aşağı kayacak
+                }                                
+            }
+            sinirEkle();
+            yilanInfo = new YilanInfo(listKare, listUzuv);
+        }
+        void newGame()
+        {
+            timer.Stop();
+            foreach (KareInfo item in listKare)
+            {
+                if (item.sinir)
+                {
+                    item.uzuvYapma();
+                    item.yemYapma();
+                }
+            }
+            gameOver = false;
+            yon = 2;
+            yemVar = false;
+            lblScore.Text = "0";
+            listUzuv.Clear();
+            yilanInfo = new YilanInfo(listKare, listUzuv);
+
+
+
+        }
+        void yemEkle()
+        {
+            if (yemVar)
+                {
+                return;
+                }
+            Random random = new Random();
+            int indis = 0;
+            bool durum = false;
+
+            while (durum == false)
+            {
+                indis = random.Next(0, toplamKareSayisi);
+                durum = true;
+
+                if (this.listKare[indis].uzuv || this.listKare[indis].sinir)
+                {
+                    durum = false;
+                }
+            }
+            if (durum)
+            {
+                this.listKare[indis].yemYap();
+                this.yemVar = true;
+            }
+
+        }
+        void sinirEkle()
+        {
+            for(int i = 0;i <= 29; i+=1)//0 dan 29 a kadar ve birer birer artıırılacak
+            {
+                listKare[i].sinirYap();
+            }
+            for (int i = 0; i <= 870; i += 30)//0 dan 870 e kadar ve otuzar şekilde artıırılacak
+            {
+                listKare[i].sinirYap();
+            }
+            for (int i = 870; i <= 899; i += 1)//870 dan 899 a kadar ve birer birer artıırılacak
+            {
+                listKare[i].sinirYap();
+            }
+            for (int i = 29; i <= 899; i += 30)//29 dan 899 a kadar ve otuzar şekilde artıırılacak
+            {
+                listKare[i].sinirYap();
+            }
+        }
+        class YilanInfo
+        {
+            public int startPosition = 33;
+            public int yon { get; set; }
+            public List<KareInfo> listKare{ get; set; }
+            public List<KareInfo> listUzuv { get; set; }
+
+            public YilanInfo(List<KareInfo> listKare, List<KareInfo> listUzuv)
+            {
+                this.listKare = listKare;
+                this.listUzuv = listUzuv;
+
+                this.listKare[31].uzuvYap();
+                this.listKare[32].uzuvYap();
+                this.listKare[33].uzuvYap();
+
+                this.listUzuv = new List<KareInfo>();
+
+                this.listUzuv.Add(this.listKare[31]);
+                this.listUzuv.Add(this.listKare[32]);
+                this.listUzuv.Add(this.listKare[33]);
+
+            }
+            public int yurut(int yon)
+            {
+                this.yon = yon;
+                switch (yon)
+                {
+                    case 1:
+                        startPosition = startPosition - 30;
+                        break;
+
+
+
+                    case 2:
+                        startPosition = startPosition +1;
+                        break;
+
+
+                    case 3:
+                        startPosition = startPosition + 30;
+                        break;
+
+
+
+                    case 4:
+                        startPosition = startPosition -1;
+                        break;
+
+
+                    default:
+                        break;
+                }
+                if (this.listKare[startPosition].uzuv || this.listKare[startPosition].sinir)
+                {
+                    return 0; //hata varsa (oyun bitiyor)
+                }
+                else
+                {
+                    this.listKare[startPosition].uzuvYap();
+                    this.listUzuv.Add(listKare[startPosition]);
+                }
+                if (this.listKare[startPosition].yem)//geldiği nokta yem ise, değil ise
+                {
+                    this.listKare[startPosition].yem = false;
+                    return 2; //yem yenmiş ise
+                }
+                else
+                {
+                    this.listKare[this.listUzuv[0].indis].uzuvYapma();
+                    this.listUzuv.RemoveAt(0);
+                    return 1;//kuyruk silinmişse
+                }
+                return 0;
+            }
+
+        }
+        class KareInfo
+        {
+            public Point location { get; set; }
+            public Size size { get; set; }
+            public Color backColor { get; set; }
+            public PictureBox pictureBox { get; set; }
+            public int indis { get; set; }
+            public bool uzuv { get; set; }
+            public bool yem { get; set; }
+            public bool sinir { get; set; }            
+            public Panel panel { get; set; }
+
+
+            public KareInfo(Panel panel, Point location, Size size, int indis)
+            //constructor:yapıcı metotlar 
+            {
+                this.panel = panel;
+                this.location = location;
+                this.size = size;
+                this.indis = indis;
+                this.backColor = Color.Black;
+                this.uzuv= false;
+                this.yem = false;
+                this.sinir = false;
+                //constructor altına özellikler set edildikten sonra pictureBox.Add(); metotunu çağırıyoruz
+
+            }
+            public void pictureBoxAdd()
+            {
+                this.pictureBox = new PictureBox(); //class altındaki pictureBox'ı kuruyoruz
+                this.pictureBox.Location = this.location;
+                this.pictureBox.Size= this.size;
+                this.pictureBox.BackColor= this.backColor;
+                this.panel.Controls.Add(this.pictureBox);//panele pictureBox ekleniyor
+            }
+            public void uzuvYap()
+            {
+                this.pictureBox.BackColor = Color.LightSeaGreen;
+                this.uzuv = true;
+            }
+            public void uzuvYapma() //bazı lokasyonlar yılanın uzvu veya yemi olabileceğinden iki ihtimal için de metot tanımlamalıyız
+            {
+                this.pictureBox.BackColor = this.backColor;
+                this.uzuv = false;
+            }
+            public void yemYap()
+            {
+                this.pictureBox.BackColor = Color.Red;
+                this.yem= true;
+            }
+            public void yemYapma()
+            {
+                this.pictureBox.BackColor = this.backColor;
+                this.yem = false;
+            }
+            public void sinirYap() //sınırlar sonradan değişmeyeceği için sınır yapma metotu eklememize gerek yok
+            {
+                this.pictureBox.BackColor = Color.LightSlateGray;
+                this.sinir = true;
+            }
+
+
+
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            yemEkle();
+            int sonuc = yilanInfo.yurut(yon);
+
+
+            switch(sonuc)
+            {
+                case 0:
+                    timer.Stop();
+                    gameOver = true;
+                    MessageBox.Show("Oyun bitti :(");
+                    break;
+
+
+                case 1:
+                    break;
+
+                case 2:
+                    yemVar = false;
+                    lblScore.Text = Convert.ToString(Convert.ToInt32(lblScore.Text) +1);
+                    break;
+
+
+                default: 
+                    break;
+            }
+        }
+
+        private void pictureRight_Click(object sender, EventArgs e)
+        {
+            if (yilanInfo.yon !=4)
+            {
+                yon = 2;
+            }
+        }
+
+        private void pictureDown_Click(object sender, EventArgs e)
+        {
+            if (yilanInfo.yon != 1)
+            {
+                yon = 3;
+            }
+        }
+
+        private void pictureLeft_Click(object sender, EventArgs e)
+        {
+            if (yilanInfo.yon != 2)
+            {
+                yon = 4;
+            }
+        }
+
+        private void pictureUp_Click(object sender, EventArgs e)
+        {
+            if (yilanInfo.yon !=3)
+            {
+                yon = 1;
+            }
+        }
+
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            if (gameOver == false)
+            { 
+                timer.Start();
+            }
+        }
+
+        private void btnDurdur_Click(object sender, EventArgs e)
+        {
+            if (gameOver== false)
+            {
+                timer.Stop();
+            }
+        }
+
+        private void lblScore_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if(keyData == Keys.Up)
+            {
+                if (yilanInfo.yon != 3)
+                {
+                    yon = 1;
+                }
+            }
+            if (keyData == Keys.Down)
+            {
+                if (yilanInfo.yon != 1)
+                {
+                    yon = 3;
+                }
+            }
+            if (keyData == Keys.Left)
+            {
+                if (yilanInfo.yon != 2)
+                {
+                    yon = 4;
+                }
+            }
+            if (keyData == Keys.Right)
+            {
+                if (yilanInfo.yon != 4)
+                {
+                    yon = 2;
+                }
+            }
+
+            return base.ProcessDialogKey(keyData);
+        }
+
+        private void btnYeniOyun_Click(object sender, EventArgs e)
+        {
+            newGame();
+        }
+    }
+}
